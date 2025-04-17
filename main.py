68f2260c34bb93d2c0b69f8e36bab88408f0040b
rrrguez <alu0101394763@ull.edu.es>
2025-04-17 16:09:29 +0100

    Implementación de logs en lugar de print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from typing import Final
 from dotenv import load_dotenv
@@ -10,72 +11,82 @@ load_dotenv()
 TOKEN: Final = os.getenv('API_KEY')
 BOT_USERNAME: Final = '@AS_OpoBot'
 
+# Configuración de los logs
+logging.basicConfig(datefmt='%d/%m/%Y %I:%M:%S %p',
+                    encoding='utf-8',
+                    filemode='w',
+                    filename='logs.log',
+                    format='%(levelname)s %(asctime)s: %(message)s',
+                    level=logging.DEBUG
+                    )
+
+logging.debug('Prueba')
 
 # Commands
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text('Hello!')
+    await update.message.reply_text('Hello!')
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text('Ayuda por favor!')
+    await update.message.reply_text('Ayuda por favor!')
 
 
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text('Custom')
+    await update.message.reply_text('Custom')
 
 
 # Responses
 
 def handle_response(text: str) -> str:
-  processed: str = text.lower()
+    processed: str = text.lower()
 
-  if 'hello' in processed:
-    return 'Hey there'
-  
-  if 'how' in processed:
-    return 'good'
-  
-  return 'No se'
+    if 'hello' in processed:
+        return 'Hey there'
+
+    if 'how' in processed:
+        return 'good'
+
+    return 'No se'
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  message_type: str = update.message.chat.type
-  text: str = update.message.text
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
 
-  print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+    logging.info(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
 
-  if message_type == 'group':
-    if BOT_USERNAME in text:
-      new_text: str = text.replace(BOT_USERNAME, '').strip
-      response: str = handle_response(new_text)
+    if message_type == 'group':
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip
+            response: str = handle_response(new_text)
+        else:
+            return
     else:
-      return
-  else:
-    response: str = handle_response(text)
-  
-  print('Bot:', response)
-  await update.message.reply_text(response)
+        response: str = handle_response(text)
+
+    logging.info(f"Bot: {response}")
+    await update.message.reply_text(response)
 
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  print(f'Update {update} caused error {context.error}')
+    logging.error('Update {update} caused error {context.error}')
 
 
 if __name__ == '__main__':
-  app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(TOKEN).build()
 
-  # Commands
-  app.add_handler(CommandHandler('start', start_command))
-  app.add_handler(CommandHandler('help', help_command))
-  app.add_handler(CommandHandler('custom', custom_command))
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
 
-  # Messages
-  app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
 
-  # Errors
-  app.add_error_handler(error)
+    # Errors
+    app.add_error_handler(error)
 
-  # Polls the bot
-  print('Polling...')
-  app.run_polling(poll_interval=3)
+    # Polls the bot
+    print('Polling...')
+    app.run_polling(poll_interval=3)
